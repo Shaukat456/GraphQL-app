ffe424d3e08155ea93f93f4c7a0f1d50b7ccefe1
Shaukat Sohail <65353819+Shaukat456@users.noreply.github.com>
2022-07-24 22:37:21 -0700

    made querries

--- server.js
+++ server.js
@@ -3,21 +3,74 @@ import { ApolloServerPluginLandingPageGraphQLPlayground} from 'apollo-server-cor
 
 
 
+//Collection  = students
+
+//doc1 //doc2 are the documetns of collection
+
+
+export const students=[
+    {
+        id:'1',
+        name:"Shaukat",
+        lastName:"sohail",
+        password:"12345"
+    },
+    {
+        id:'2',
+        name:"Ahmed",
+        lastName:"iqbal",
+        password:"654987"
+    }
+]
+
+
+//Collection  = quotes
+
+export const quotes=[
+
+    {
+        name:"this is a quote by me ",
+        by:'1'  // by which user   // refers to the id of the student 
+    },
+    {
+        name:"hello there is not a quote ",
+        by:'2'
+    }
+]
+
+
+
 const typeDefs = gql
 `
- type Query{
-    msg:String
- }   
+type  Query{
+    students : [Students]
+    quotes : [Quotes]
+}
+type  Query{
+    msg : String
+}
+ type Students{
+     id:ID   
+     name:String 
+     lastName:String 
+     password:String 
+}
 
-
+type Quotes{
+    name:String
+    by:String
+}
 `
 
 
 const resolvers_obj={
     Query:{
-        msg:()=>{
-            return 'A msg '
-        }
+        // msg:()=>{
+        //     return 'A msg '
+        // }
+        students:()=>students,
+        quotes:()=>quotes
+
     }
 }
 
